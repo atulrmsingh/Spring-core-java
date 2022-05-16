82d8e325e5a7b889439e8cd224634bfaf55afb5d
Atul singh <Atul singh@Atul>
2022-05-16 11:37:13 +0530

    initial commit

---
+++ src/main/java/com/spring/ioc/Jio.java
@@ -0,0 +1,15 @@
+package com.spring.ioc;
+
+public class Jio implements Sim {
+
+	public void call() {
+		System.out.println("calling using jio");
+		
+	}
+
+	public void data() {
+		System.out.println("using internet by jio");
+		
+	}
+
+}
