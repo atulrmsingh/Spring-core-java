d487a6dfc663109bc0b5374593e1a66578d201fa
atulrmsingh <atulrmsingh@gmail.com>
2022-05-18 15:17:55 +0530

    Added annotation based dependencyInjection also done by type and by name and modified annotationBeans.xml file.

--- src/main/java/com/spring/annotationDependency/Person.java
+++ src/main/java/com/spring/annotationDependency/Person.java
@@ -1,19 +1,20 @@
 package com.spring.annotationDependency;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
 public class Person {
+	@Autowired
+	@Qualifier("heartObject1")
 	Heart heart;
 
-	public void setHeart(Heart heart) {
-		this.heart = heart;
-	}
-
 	public void pumping() {
-		if(heart != null) {
-		
-		heart.pump();
-	}else {
-		System.out.println("you are dead");
-	}
-		
+		if (heart != null) {
+
+			heart.pump();
+		} else {
+			System.out.println("you are dead");
+		}
+
 	}
 }
